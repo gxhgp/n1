252d89ce2262eaf51771470cdfcf6e9f7628e342
gxhgp <76831545+gxhgp@users.noreply.github.com>
2024-03-13 12:49:24 +0800

    Update getipAtv.sh

--- getipAtv.sh
+++ getipAtv.sh
@@ -31,7 +31,8 @@ getadr(){
     # echo "" >> $tvfile
     s=$3
     e=$4
-    #echo "$combined_result" > $file_path3
+    echo "$combined_result" > $file_path3
+    cat $file_path3
     #sed -i "${s},${e} c ${combined_result}" $tvfile
     sed -i "${s},${e}d" $tvfile
     sed -i "${s}r $file_path3" $tvfile
